58143dd2e459ca30689f80a2d734af247bd1bd79
subratrout <subratnrout@gmail.com>
2017-12-25 00:56:01 -0700

    Highlighting the  added line_item upon addding to cart using jquery-ui

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -15,14 +15,15 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create line_item" do
+  test "should create line_item via ajax" do
     assert_difference('LineItem.count') do
-      post line_items_url, params: { product_id: products(:ruby).id }
+      post line_items_url, params: { product_id: products(:ruby).id }, xhr: true
     end
 
-    follow_redirect!
-    assert_select 'h2', 'Your shopping cart'
-    assert_select 'td', 'Programming Ruby 1.9​'
+    assert_response :success
+    assert_select_jquery :html,'#cart' do
+      assert_select 'tr#current_item td',/Programming Ruby 1.9/
+    end
   end
 
   test "should show line_item" do
