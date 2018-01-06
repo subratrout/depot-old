93fa90cf48f4f115396bc617f46d5d62675ecfe4
subratrout <subratnrout@gmail.com>
2018-01-06 15:00:59 -0700

    Setting up en or es locale using i18n

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,14 @@ Rails.application.routes.draw do
     delete 'logout' => 'sessions#destroy'
   end
 
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
+  scope '(:locale)' do
+    resources :users
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index'
+  end
+  
 
   get 'store/index'
 
