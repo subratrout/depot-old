c1b4899b886e9c692b930603cdd0698b36f11db2
subratrout <subratnrout@gmail.com>
2018-01-06 21:38:18 -0700

    Adding a local switcher for i18n

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     resources :orders
     resources :line_items
     resources :carts
-    root 'store#index', as: 'store_index'
+    root 'store#index', as: 'store_index', via: :all
   end
   
 
